f0042fb2f47d7354971ecd0a373aa99482087708
Cesar Diaz <llalejoll11@gmail.com>
2021-08-05 23:14:26 -0500

    creando el primero controlador

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home/{name}',function($name){
-    return view('home',['name' => $name]);
-    // return "holi mundex";
-});
+Route::get('/admin/{name}', 'AdminController@index');
